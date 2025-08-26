942c3c2caa11ef43588f45ea6330823dcebcdb04
duswo519-beep <duswo519@gmail.com>
2025-08-26 23:00:39 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-#import streamlit as st
+import streamlit as st
 #from dotenv import load_dotenv
-load_dotenv()
+#load_dotenv()
 
 #ChatOpenAI 초기화
 llm = init_chat_model("gpt-4o-mini", model_provider="openai")
